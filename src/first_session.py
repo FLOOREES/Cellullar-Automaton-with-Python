78d91d5134ac7e12e80b559638c26d8837a51083
FLOOREES <117522350+FLOOREES@users.noreply.github.com>
2024-04-28 10:21:32 +0200

    first_setup

---
+++ src/first_session.py
@@ -0,0 +1,14 @@
+"""
+En aquesta sessió generarem un codi en Python, R o C++ que ens permeti implementar un autòmat cel·lular que segueixi les regles de wolfram,
+ definides a:
+
+https://mathworld.wolfram.com/ElementaryCellularAutomaton.html
+
+Podeu i de fet, es recomana, que empreu alguna IA com Copilot o ChatGPT, per agilitzar la generació de codi.
+
+Un cop fet això es generalitzarà l'autòmat per permetre combinar diferents regles en un mateix autòmat, generant una estructura multicapa,
+ com la presentada a teoria.
+
+Al final de la sessió hauríem de tenir un autòmat que pogués implementar les regles bàsiques i mostrar l'evolució de forma temporal en un
+ gràfic per una regla simple o per una combinació de elles.
+"""
